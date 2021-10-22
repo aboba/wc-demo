c236c4605c0a976756f519b70898ad8777242acf
Bernard Aboba <bernard.aboba@gmail.com>
2021-10-22 11:14:59 -0700

    Comment out some logging

--- js/stream_worker.js
+++ js/stream_worker.js
@@ -136,12 +136,12 @@ SSRC = this.config.ssrc
          writeUInt32(hdr, 12, chunk.deltaframeIndex);
          writeUInt64(hdr, 16, BigInt(chunk.timestamp));
          writeUInt32(hdr, 24, config.ssrc);
-         self.postMessage({text: 'Serial B0: ' + B0 + ' B1: ' + B1 + ' B2: ' + B2 + ' B3: ' + B3});
-         self.postMessage({text: 'Serial seq: ' + chunk.seqNo + ' kf: ' + chunk.keyframeIndex + ' delta: ' + chunk.deltaframeIndex + ' dur: ' + chunk.duration + ' ts: ' + chunk.timestamp + ' ssrc: ' + config.ssrc +  ' pt: ' + pt + ' tid: ' + tid + ' type: ' + chunk.type + ' discard: ' + d + ' base: ' + b});
+         // self.postMessage({text: 'Serial B0: ' + B0 + ' B1: ' + B1 + ' B2: ' + B2 + ' B3: ' + B3});
+         // self.postMessage({text: 'Serial seq: ' + chunk.seqNo + ' kf: ' + chunk.keyframeIndex + ' delta: ' + chunk.deltaframeIndex + ' dur: ' + chunk.duration + ' ts: ' + chunk.timestamp + ' ssrc: ' + config.ssrc +  ' pt: ' + pt + ' tid: ' + tid + ' type: ' + chunk.type + ' discard: ' + d + ' base: ' + b});
          if (chunk.type == "config") {
            let enc = new TextEncoder();
            const cfg = enc.encode(chunk.config); 
-           self.postMessage({text: 'Serial Config: ' + chunk.config + ' Length: ' + cfg.length});
+           // self.postMessage({text: 'Serial Config: ' + chunk.config + ' Length: ' + cfg.length});
            let result = new Uint8Array( hdr.byteLength + cfg.length);
            result.set(new Uint8Array(hdr), 0);
            result.set(new Uint8Array(cfg), hdr.byteLength);
@@ -152,7 +152,7 @@ SSRC = this.config.ssrc
            let data = new ArrayBuffer(chunk.byteLength);
            chunk.copyTo(data);
            result.set(new Uint8Array(data), hdr.byteLength);
-           self.postMessage({text: 'Serial hdr: ' + hdr.byteLength + ' chunk length: ' + chunk.byteLength + ' result length: ' + result.byteLength});
+           // self.postMessage({text: 'Serial hdr: ' + hdr.byteLength + ' chunk length: ' + chunk.byteLength + ' result length: ' + result.byteLength});
            controller.enqueue(result.buffer);
          }
       }
@@ -179,7 +179,7 @@ SSRC = this.config.ssrc
          const B1 = (first4 & 0x0000FF00) >> 8;
          const B2 = (first4 & 0x00FF0000) >> 16;
          const B3 = (first4 & 0xFF000000) >> 24; 
-         self.postMessage({text: 'Deserial B0: ' + B0 + ' B1: ' + B1 + ' B2: ' + B2 + ' B3: ' + B3});
+         // self.postMessage({text: 'Deserial B0: ' + B0 + ' B1: ' + B1 + ' B2: ' + B2 + ' B3: ' + B3});
          const lid = B0;
          const pt =  B2;
          const tid = (B1 & 0xE0) >> 5;
@@ -197,7 +197,7 @@ SSRC = this.config.ssrc
            };
            let dec = new TextDecoder();
            hydChunk.config = dec.decode(new Uint8Array(chunk, 28));
-           self.postMessage({text: 'Deserial Config: ' + hydChunk.config});
+           // self.postMessage({text: 'Deserial Config: ' + hydChunk.config});
          } else {
            let data = new Uint8Array(chunk.byteLength - 28); //create Uint8Array for data
            data.set(new Uint8Array(chunk, 28));
@@ -213,8 +213,8 @@ SSRC = this.config.ssrc
          hydChunk.seqNo = seqNo;
          hydChunk.keyframeIndex = keyframeIndex;
          hydChunk.deltaframeIndex = deltaframeIndex;
-         self.postMessage({text: 'Deserial hdr: 28 ' + 'chunk length: ' + chunk.byteLength });
-         self.postMessage({text: 'Deserial seq: ' + hydChunk.seqNo + ' kf: ' + hydChunk.keyframeIndex + ' delta: ' + hydChunk.deltaframeIndex + ' dur: ' + hydChunk.duration + ' ts: ' + hydChunk.timestamp + ' ssrc: ' + hydChunk.ssrc + ' pt: ' + hydChunk.pt + ' tid: ' + tid + ' type: ' + hydChunk.type});
+         // self.postMessage({text: 'Deserial hdr: 28 ' + 'chunk length: ' + chunk.byteLength });
+         // self.postMessage({text: 'Deserial seq: ' + hydChunk.seqNo + ' kf: ' + hydChunk.keyframeIndex + ' delta: ' + hydChunk.deltaframeIndex + ' dur: ' + hydChunk.duration + ' ts: ' + hydChunk.timestamp + ' ssrc: ' + hydChunk.ssrc + ' pt: ' + hydChunk.pt + ' tid: ' + tid + ' type: ' + hydChunk.type});
          controller.enqueue(hydChunk);
        }
      });
