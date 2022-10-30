b0576109622bd5f6dd8dc71ccb4296fb84e81c1d
Bernard Aboba <bernard.aboba@gmail.com>
2022-10-29 21:33:53 -0700

    Remove encoder and decoder time measurements, fix cfg.svc bug

--- js/stream_worker.js
+++ js/stream_worker.js
@@ -1,21 +1,6 @@
 'use strict';
 
 let encoder, decoder, pl, started = false, stopped = false;
-let enc_aggregate = {
-  all: [],
-  min: Number.MAX_VALUE,
-  max: 0,
-  avg: 0,
-  sum: 0,
-};
-
-let dec_aggregate = {
-  all: [],
-  min: Number.MAX_VALUE,
-  max: 0,
-  avg: 0,
-  sum: 0,
-};
 
 let encqueue_aggregate = {
   all: [],
@@ -33,40 +18,11 @@ let decqueue_aggregate = {
   sum: 0,
 };
 
-function enc_update(duration) {
-  enc_aggregate.all.push(duration);
-  enc_aggregate.min = Math.min(enc_aggregate.min, duration);
-  enc_aggregate.max = Math.max(enc_aggregate.max, duration);
-  enc_aggregate.sum += duration;
-}
-
 function encqueue_update(duration) {
   encqueue_aggregate.all.push(duration);
   encqueue_aggregate.min = Math.min(encqueue_aggregate.min, duration);
   encqueue_aggregate.max = Math.max(encqueue_aggregate.max, duration);
   encqueue_aggregate.sum += duration;
-}
-
-function enc_report() {
-  enc_aggregate.all.sort();
-  const len = enc_aggregate.all.length;
-  const half = len >> 1;
-  const f = (len + 1) >> 2;
-  const t = (3 * (len + 1)) >> 2;
-  const alpha1 = (len + 1)/4 - Math.trunc((len + 1)/4);
-  const alpha3 = (3 * (len + 1)/4) - Math.trunc(3 * (len + 1)/4);
-  const fquart = enc_aggregate.all[f] + alpha1 * (enc_aggregate.all[f + 1] - enc_aggregate.all[f]);
-  const tquart = enc_aggregate.all[t] + alpha3 * (enc_aggregate.all[t + 1] - enc_aggregate.all[t]);
-  const median = len % 2 === 1 ? enc_aggregate.all[len >> 1] : (enc_aggregate.all[half - 1] + enc_aggregate.all[half]) / 2;
-  return {
-     count: len,
-     min: enc_aggregate.min,
-     fquart: fquart,
-     avg: enc_aggregate.sum / len,
-     median: median,
-     tquart: tquart,
-     max: enc_aggregate.max,
-  };
 }
 
 function encqueue_report() {
@@ -91,40 +47,11 @@ function encqueue_report() {
   };
 }
 
-function dec_update(duration) {
-   dec_aggregate.all.push(duration);
-   dec_aggregate.min = Math.min(dec_aggregate.min, duration);
-   dec_aggregate.max = Math.max(dec_aggregate.max, duration);
-   dec_aggregate.sum += duration;
-}
-
 function decqueue_update(duration) {
    decqueue_aggregate.all.push(duration);
    decqueue_aggregate.min = Math.min(decqueue_aggregate.min, duration);
    decqueue_aggregate.max = Math.max(decqueue_aggregate.max, duration);
    decqueue_aggregate.sum += duration;
-}
-
-function dec_report() {
-  dec_aggregate.all.sort();
-  const len = dec_aggregate.all.length;
-  const half = len >> 1;
-  const f = (len + 1) >> 2;
-  const t = (3 * (len + 1)) >> 2;
-  const alpha1 = (len + 1)/4 - Math.trunc((len + 1)/4);
-  const alpha3 = (3 * (len + 1)/4) - Math.trunc(3 * (len + 1)/4);
-  const fquart = dec_aggregate.all[f] + alpha1 * (dec_aggregate.all[f + 1] - dec_aggregate.all[f]);
-  const tquart = dec_aggregate.all[t] + alpha3 * (dec_aggregate.all[t + 1] - dec_aggregate.all[t]);
-  const median = len % 2 === 1 ? dec_aggregate.all[len >> 1] : (dec_aggregate.all[half - 1] + dec_aggregate.all[half]) / 2;
-  return {
-     count: len,
-     min: dec_aggregate.min,
-     fquart: fquart,
-     avg: dec_aggregate.sum / len,
-     median: median,
-     tquart: tquart,
-     max: dec_aggregate.max,
-  };
 }
 
 function decqueue_report() {
@@ -212,11 +139,7 @@ class pipeline {
              try {
                const queue = this.decoder.decodeQueueSize;
                decqueue_update(queue);
-               const before = performance.now();
                this.decoder.decode(chunk);
-               const after = performance.now();
-               const duration = after - before;
-               dec_update(duration);
              } catch (e) {
                self.postMessage({severity: 'fatal', text: 'Derror size: ' + chunk.byteLength + ' seq: ' + chunk.seqNo + ' kf: ' + chunk.keyframeIndex + ' delta: ' + chunk.deltaframeIndex + ' dur: ' + chunk.duration + ' ts: ' + chunk.timestamp + ' ssrc: ' + chunk.ssrc + ' pt: ' + chunk.pt + ' tid: ' + chunk.temporalLayerId + ' type: ' + chunk.type});
                self.postMessage({severity: 'fatal', text: `Catch Decode error: ${e.message}`});
@@ -253,7 +176,7 @@ class pipeline {
                controller.enqueue(configChunk); 
              } 
              chunk.temporalLayerId = 0;
-             if (cfg.svc.temporalLayerId) {
+             if (cfg.svc) {
                chunk.temporalLayerId = cfg.svc.temporalLayerId;
              }
              this.seqNo++;
@@ -294,11 +217,7 @@ class pipeline {
              if (this.encoder.state != "closed") {
                const queue = this.encoder.encodeQueueSize;
                encqueue_update(queue);
-               const before = performance.now();
                this.encoder.encode(frame, { keyFrame: insert_keyframe });
-               const after = performance.now();
-               const duration = after - before;
-               enc_update(duration);
              } 
            } catch(e) {
              self.postMessage({severity: 'fatal', text: 'Encoder Error: ' + e.message});
@@ -310,13 +229,9 @@ class pipeline {
    }
 
    stop() {
-     const enc_stats = enc_report();
      const encqueue_stats = encqueue_report();
-     const dec_stats = dec_report();
      const decqueue_stats = decqueue_report();
-     self.postMessage({text: 'Encoder Time report: ' + JSON.stringify(enc_stats)});
      self.postMessage({text: 'Encoder Queue report: ' + JSON.stringify(encqueue_stats)});
-     self.postMessage({text: 'Decoder Time report: ' + JSON.stringify(dec_stats)});
      self.postMessage({text: 'Decoder Queue report: ' + JSON.stringify(decqueue_stats)});
      if (stopped) return;
      stopped = true;
