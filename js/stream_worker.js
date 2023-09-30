bcb3ade75012b2d7f7b2657147c9d384628c28ee
Bernard Aboba <bernard.aboba@gmail.com>
2023-09-30 12:16:34 -0700

    Fix bug in setting optimizeForLatency

--- js/stream_worker.js
+++ js/stream_worker.js
@@ -252,7 +252,7 @@ class pipeline {
            output: (chunk, cfg) => {
              if (cfg.decoderConfig) {
                cfg.decoderConfig.hardwareAcceleration = config.decHwAcceleration;
-               cfg.decoderConfig.optimizeForLatency = (config.latencyPref == "realtime");
+               cfg.decoderConfig.optimizeForLatency = (config.latencyMode == "realtime");
                const decoderConfig = JSON.stringify(cfg.decoderConfig);
                self.postMessage({text: 'Configuration: ' + decoderConfig});
                const configChunk =
