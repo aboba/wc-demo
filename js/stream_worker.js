3709d310076179306abfb5093dd4df1ec0f3fe2c
Bernard Aboba <bernard.aboba@gmail.com>
2023-09-30 11:26:14 -0700

    Change default mode and codeclink to C

--- js/stream_worker.js
+++ js/stream_worker.js
@@ -252,8 +252,7 @@ class pipeline {
            output: (chunk, cfg) => {
              if (cfg.decoderConfig) {
                cfg.decoderConfig.hardwareAcceleration = config.decHwAcceleration;
-               cfg.decoderConfig.optimizeForLatency = true;
-               if (config.latencyPref == 'quality') cfg.decoderConfig.optimizeForLatency = false;
+               cfg.decoderConfig.optimizeForLatency = !(config.latencyPref == 'quality');
                const decoderConfig = JSON.stringify(cfg.decoderConfig);
                self.postMessage({text: 'Configuration: ' + decoderConfig});
                const configChunk =
