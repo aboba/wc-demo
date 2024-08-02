524fab6405e24cb5b7fb8ccfe36fa21b37355980
Bernard Aboba <bernard.aboba@gmail.com>
2024-08-02 14:38:02 -0700

    Update HEVC profile
    Updates HEVC profile to 4.0 from 3.1.

--- js/main.js
+++ js/main.js
@@ -391,8 +391,8 @@ document.addEventListener('DOMContentLoaded', async function(event) {
           config.pt = 1;
           break;
         case "H265":
-          // config.codec = "hvc1.1.6.L123.00"; // Main profile, level 4.1, main Tier
-          config.codec = "hev1.1.6.L93.B0"; // Main profile, level 3.1, up to 1280 x 720@33.7
+          config.codec = "hvc1.1.6.L120.00"; // Main profile, level 4.0, main Tier
+          // config.codec = "hev1.1.6.L93.B0"; // Main profile, level 3.1, up to 1280 x 720@33.7
           config.hevc = { format: "annexb" };
           config.pt = 2;
           break;
